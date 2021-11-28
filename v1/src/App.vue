e2b5dd52f2694ccccf5a17fc833e116745e97190
gracale <acdepartmentwarning@gmail.com>
2021-11-28 23:05:33 +0800

    安装并初始化vue-router

--- v1/src/App.vue
+++ v1/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>hi</div>
+  <div>导航栏 | <router-link to='/'>Frank</router-link> | <router-link to="/xxx">Frank2</router-link></div>
+  <hr/>
   <router-view />
 </template>
 
--- v1/src/main.ts
+++ v1/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory,createRouter} from 'vue-router'
-import Frank from './component/Frank.vue'
+import Frank from './components/Frank.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
