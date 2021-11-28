c38d5382182cae3395428bfa7f7dfc077ff3a1b3
gracale <acdepartmentwarning@gmail.com>
2021-11-29 02:38:40 +0800

    调节topnav和aside位置

--- v1/src/components/Topnav.vue
+++ v1/src/components/Topnav.vue
@@ -8,10 +8,12 @@
         </div>
 </template>
 <style lang="scss" scoped>
-    .topnav {
+.topnav {
   background: pink;
   display: flex;
   padding: 16px;
+  position: relative;
+  z-index: 10;
   > .logo {
     max-width: 6em;
     margin-right: auto;
