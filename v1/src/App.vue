a65c4ff0545b6e4cfd293aa3630dab61b789dc64
gracale <acdepartmentwarning@gmail.com>
2021-11-29 18:04:36 +0800

    路由切换之后关闭 aside

--- v1/src/App.vue
+++ v1/src/App.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import { ref,provide } from "vue"
+import { router } from "./router";
 
 export default {
   name: 'App',
@@ -12,6 +13,12 @@ export default {
     const width = document.documentElement.clientWidth
     const menuVisible = ref(width <= 500 ? false:true);
     provide('menuVisible',menuVisible)
+    router.afterEach(()=>{
+      if (width <= 500){
+        //每次切换路由 侧边栏都会关闭
+        menuVisible.value = false;
+      }
+    })
   }
 }
 </script>
