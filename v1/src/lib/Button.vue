1920274c595454c534bb8aa1b72c133fb66f2a15
gracale <acdepartmentwarning@gmail.com>
2021-12-06 21:44:25 +0800

    加载中转圈的css

--- v1/src/lib/Button.vue
+++ v1/src/lib/Button.vue
@@ -1,40 +1,45 @@
 <template>
-  <button class="v1-button" :class="classes" :disabled="disabled">
-    <slot />
-  </button>
+    <button class="v1-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="v1-loadingIndicator"></span>
+        <slot />
+    </button>
 </template>
 <script lang="ts">
 import { computed } from "vue";
 export default {
-  props: {
-    theme: {
-      type: String,
-      default: "button",
+    props: {
+        theme: {
+            type: String,
+            default: "button",
+        },
+        size: {
+            type: String,
+            default: "normal",
+        },
+        level: {
+            type: String,
+            default: "normal",
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        loading: {
+            type: Boolean,
+            default: false,
+        }
     },
-    size: {
-      type: String,
-      default: "normal",
+    setup(props) {
+        const { theme, size, level } = props;
+        const classes = computed(() => {
+            return {
+                [`v1-theme-${theme}`]: theme,
+                [`v1-size-${size}`]: size,
+                [`v1-level-${level}`]: level,
+            };
+        });
+        return { classes };
     },
-    level: {
-      type: String,
-      default: "normal",
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props) {
-    const { theme, size, level } = props;
-    const classes = computed(() => {
-      return {
-        [`v1-theme-${theme}`]: theme,
-        [`v1-size-${size}`]: size,
-        [`v1-level-${level}`]: level,
-      };
-    });
-    return { classes };
-  },
 };
 </script>
 <style lang="scss">
@@ -46,123 +51,143 @@ $radius: 4px;
 $red: red;
 $grey: grey;
 .v1-button {
-  box-sizing: border-box;
-  height: $h;
-  padding: 0 12px;
-  cursor: pointer;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  background: white;
-  color: $color;
-  border: 1px solid $border-color;
-  border-radius: $radius;
-  box-shadow: 0 1px 0 fade-out(black, 0.95);
-  transition: background 250ms;
-  & + & {
-    margin-left: 8px;
-  }
-  &:hover,
-  &:focus {
-    color: $blue;
-    border-color: $blue;
-  }
-  &:focus {
-    outline: none;
-  }
-  &::-moz-focus-inner {
-    border: 0;
-  }
-  &.v1-theme-link {
-    border-color: transparent;
-    box-shadow: none;
-    color: $blue;
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
+    & + & {
+        margin-left: 8px;
+    }
     &:hover,
     &:focus {
-      color: lighten($blue, 10%);
+        color: $blue;
+        border-color: $blue;
     }
-  }
-  &.v1-theme-text {
-    border-color: transparent;
-    box-shadow: none;
-    color: inherit;
-    &:hover,
     &:focus {
-      background: darken(white, 5%);
+        outline: none;
     }
-  }
-  &.v1-size-big {
-    font-size: 24px;
-    height: 48px;
-    padding: 0 16px;
-  }
-  &.v1-size-small {
-    font-size: 12px;
-    height: 20px;
-    padding: 0 4px;
-  }
-  &.v1-theme-button {
-    &.v1-level-main {
-      background: $blue;
-      color: white;
-      border-color: $blue;
-      &:hover,
-      &:focus {
-        background: darken($blue, 10%);
-        border-color: darken($blue, 10%);
-      }
+    &::-moz-focus-inner {
+        border: 0;
     }
-    &.v1-level-danger {
-      background: $red;
-      border-color: $red;
-      color: white;
-      &:hover,
-      &:focus {
-        background: darken($red, 10%);
-        border-color: darken($red, 10%);
-      }
+    &.v1-theme-link {
+        border-color: transparent;
+        box-shadow: none;
+        color: $blue;
+        &:hover,
+        &:focus {
+            color: lighten($blue, 10%);
+        }
     }
-  }
-  &.v1-theme-link {
-    &.v1-level-danger {
-      color: $red;
-      &:hover,
-      &:focus {
-        color: darken($red, 10%);
-      }
+    &.v1-theme-text {
+        border-color: transparent;
+        box-shadow: none;
+        color: inherit;
+        &:hover,
+        &:focus {
+            background: darken(white, 5%);
+        }
     }
-  }
-  &.v1-theme-text {
-    &.v1-level-main {
-      color: $blue;
-      &:hover,
-      &:focus {
-        color: darken($blue, 10%);
-      }
+    &.v1-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
     }
-    &.v1-level-danger {
-      color: $red;
-      &:hover,
-      &:focus {
-        color: darken($red, 10%);
-      }
+    &.v1-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
     }
-  }
-  &.v1-theme-button {
-    &[disabled] {
-      cursor: not-allowed;
-      color: $grey;
-      &:hover {
-        border-color: $grey;
-      }
+    &.v1-theme-button {
+        &.v1-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+        &.v1-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
     }
-  }
-  &.v1-theme-link, &.v1-theme-text {
-    &[disabled] {
-      cursor: not-allowed;
-      color: $grey;
+    &.v1-theme-link {
+        &.v1-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
     }
-  }
+    &.v1-theme-text {
+        &.v1-level-main {
+            color: $blue;
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+        &.v1-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+    &.v1-theme-button {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+            &:hover {
+                border-color: $grey;
+            }
+        }
+    }
+    &.v1-theme-link,
+    &.v1-theme-text {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+        }
+    }
+    > .v1-loadingIndicator {
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        margin-right: 4px;
+        border-radius: 8px;
+        border-color: $blue $blue $blue transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: v1-spin 1s infinite linear;
+    }
+}
+@keyframes v1-spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
 }
 </style>
