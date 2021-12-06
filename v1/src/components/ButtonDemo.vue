b7ccf2e33bd83623c7768f569b176ffe064b6635
gracale <acdepartmentwarning@gmail.com>
2021-12-06 19:58:17 +0800

    添加theme属性

--- v1/src/components/ButtonDemo.vue
+++ v1/src/components/ButtonDemo.vue
@@ -2,21 +2,21 @@
     <div>Button 示例</div>
     <h1>实例1</h1>
     <div>
-        <Button @click="onClick"
-        @focus="onClick"
-        @mouseover="onClick"
-        >你好</Button>
+        <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
     </div>
 </template>
 <script lang='ts'>
 import Button from '../lib/Button.vue'
-export default{
-    components:{Button},
+export default {
+    components: { Button },
     setup() {
-        const onClick = ()=>{
+        const onClick = () => {
             console.log('hi')
         }
-        return {onClick}
+        return { onClick }
     }
 }
 </script>
--- v1/src/lib/Button.vue
+++ v1/src/lib/Button.vue
@@ -1,22 +1,20 @@
 <template>
-<div :size="size">
-    <button v-bind="rest">
+    <button class="v1-button" :class="{ [`theme-${theme}`]: theme }">
         <slot />
     </button>
-</div>
 </template>
 <script lang="ts">
-export default{
-    inheritAttrs:false, //div不再默认获得ButtonDemo的绑定事件了
-    setup(props, context) {
-        const {size, ...rest} = context.attrs
-        //ES6 剩余操作符 size和其他属性分离，div拿size，button拿剩下
-        return{size,rest}
+export default {
+    props: {
+        theme: {
+            type: String,
+            default:'button'
+        }
     }
 };
 </script>
 <style lang="scss" scoped>
-    div{
-        border: 1px solid red;
-    }
+div {
+    border: 1px solid red;
+}
 </style>
