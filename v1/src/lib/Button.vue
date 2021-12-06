6ebb2acfab9cbfaa98fd91bfce49065064f0d897
gracale <acdepartmentwarning@gmail.com>
2021-12-06 21:17:04 +0800

    支持level属性

--- v1/src/lib/Button.vue
+++ v1/src/lib/Button.vue
@@ -15,17 +15,26 @@ export default {
             type: String,
             default: "normal",
         },
+        level: {
+            type: String,
+            default: "normal",
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
     },
-setup(props) {
-    const { theme, size } = props;
-    const classes = computed(() => {
-        return {
-            [`v1-theme-${theme}`]: theme,
-            [`v1-size-${size}`]: size,
-        }
-    })
-    return { classes };
-}
+    setup(props) {
+        const { theme, size, level } = props;
+        const classes = computed(() => {
+            return {
+                [`v1-theme-${theme}`]: theme,
+                [`v1-size-${size}`]: size,
+                [`v1-level-${level}`]: level,
+            }
+        })
+        return { classes };
+    }
 };
 </script>
 <style lang="scss">
@@ -33,64 +42,111 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
 $radius: 4px;
 .v1-button {
-    box-sizing: border-box;
-    height: $h;
-    padding: 0 12px;
-    cursor: pointer;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    white-space: nowrap;
-    background: white;
-    color: $color;
-    border: 1px solid $border-color;
-    border-radius: $radius;
-    box-shadow: 0 1px 0 fade-out(black, 0.95);
-    & + & {
-        margin-left: 8px;
-    }
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.v1-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
     &:hover,
     &:focus {
-        color: $blue;
-        border-color: $blue;
+      color: lighten($blue, 10%);
     }
+  }
+  &.v1-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
     &:focus {
-        outline: none;
+      background: darken(white, 5%);
     }
-    &::-moz-focus-inner {
-        border: 0;
+  }
+  &.v1-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.v1-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.v1-theme-button {
+    &.v1-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.v1-theme-link {
-        border-color: transparent;
-        box-shadow: none;
-        color: $blue;
-        &:hover,
-        &:focus {
-            color: lighten($blue, 10%);
-        }
+    &.v1-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
     }
-    &.v1-theme-text {
-        border-color: transparent;
-        box-shadow: none;
-        color: inherit;
-        &:hover,
-        &:focus {
-            background: darken(white, 5%);
-        }
+  }
+  &.v1-theme-link {
+    &.v1-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
-    &.v1-theme-button {
-        &.v1-size-big {
-            font-size: 24px;
-            height: 48px;
-            padding: 0 16px;
-        }
-        &.v1-size-small {
-            font-size: 12px;
-            height: 20px;
-            padding: 0 4px;
-        }
+  }
+  &.v1-theme-text {
+    &.v1-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
     }
+    &.v1-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 }
 </style> 
