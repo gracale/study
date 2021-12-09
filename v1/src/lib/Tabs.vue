dad1a9a14cac55f599dc9accffa562e1b9aeb27d
gracale <acdepartmentwarning@gmail.com>
2021-12-10 00:25:06 +0800

    tab完成

--- v1/src/lib/Tabs.vue
+++ v1/src/lib/Tabs.vue
@@ -1,30 +1,79 @@
 <template>
     <div class="v1-tabs">
-        <div class="v1-tabs-nav">
-            <div class="v1-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+        <div class="v1-tabs-nav" ref="container">
+            <div
+                class="v1-tabs-nav-item"
+                v-for="(t,index) in titles"
+                :ref="el => { if (t === selected) selectedItem = el }"
+                @click="select(t)"
+                :class="{ selected: t === selected }"
+                :key="index"
+            >{{ t }}</div>
+            <div class="v1-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="v1-tabs-content">
-            <component class="v1-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+            <component :is="current" :key="current.props.title" />
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
+import {
+    computed,
+    ref,
+    watchEffect,
+    onMounted
+} from 'vue'
 export default {
+    props: {
+        selected: {
+            type: String
+        }
+    },
     setup(props, context) {
+        const selectedItem = ref<HTMLDivElement>(null)
+        const indicator = ref<HTMLDivElement>(null)
+        const container = ref<HTMLDivElement>(null)
+        onMounted(() => {
+            watchEffect(() => {
+                const {
+                    width
+                } = selectedItem.value.getBoundingClientRect()
+                indicator.value.style.width = width + 'px'
+                const {
+                    left: left1
+                } = container.value.getBoundingClientRect()
+                const {
+                    left: left2
+                } = selectedItem.value.getBoundingClientRect()
+                const left = left2 - left1
+                indicator.value.style.left = left + 'px'
+            })
+        })
         const defaults = context.slots.default()
         defaults.forEach((tag) => {
             if (tag.type !== Tab) {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
         })
+        const current = computed(() => {
+            return defaults.find(tag => tag.props.title === props.selected)
+        })
         const titles = defaults.map((tag) => {
             return tag.props.title
-        });
+        })
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+        }
         return {
+            current,
             defaults,
-            titles
+            titles,
+            select,
+            selectedItem,
+            indicator,
+            container
         }
     }
 }
@@ -35,24 +84,34 @@ $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
 .v1-tabs {
-  &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-      &:first-child {
-        margin-left: 0;
-      }
-      &.selected {
-        color: $blue;
-      }
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+        position: relative;
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+            &:first-child {
+                margin-left: 0;
+            }
+            &.selected {
+                color: $blue;
+            }
+        }
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            width: 100px;
+            transition: all 250ms;
+        }
     }
-  }
-  &-content {
-    padding: 8px 0;
-  }
+    &-content {
+        padding: 8px 0;
+    }
 }
 </style>
