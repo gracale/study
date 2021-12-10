5f289a4528b92c8d317bb40b14cc0072d2d20a80
gracale <acdepartmentwarning@gmail.com>
2021-12-11 00:53:56 +0800

    更新menu

--- v1/src/components/Topnav.vue
+++ v1/src/components/Topnav.vue
@@ -6,15 +6,24 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+    <li>
+      <router-link to ="/doc">文档</router-link>
+    </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from 'vue';
 export default {
+  props:{
+    toggleMenuButtonVisible:{
+      type:Boolean,
+      default:false
+    }
+  },
   setup() {
     const menuVisible = inject<Ref<boolean>>('menuVisible')
     const toggleMenu = () => {
@@ -56,7 +65,6 @@ $color: #007974;
   > .toggleAside {
     width: 24px;
     height: 24px;
-
     position: absolute;
     left: 16px;
     top: 50%;
--- v1/src/views/Home.vue
+++ v1/src/views/Home.vue
@@ -1,51 +1,97 @@
 <template>
-  <div>
-    <div class="topnavAndBanner">
-      <Topnav />
-      <div class="banner">
-        <h1>V1 UI</h1>
-        <h2>一个厉害的 UI 框架</h2>
-        <p class="actions">
-          <a href="https://github.com/gracale/test/blob/main/v1/index.html">GitHub</a>
-          <router-link to="/doc">开始</router-link>
-        </p>
-      </div>
-    </div>
-    <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-Vue" />
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts" />
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-code" />
-      </svg>
+<div>
+  <div class="topnavAndBanner">
+    <Topnav />
+    <div class="banner">
+      <h1>V1 UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com/gracale/study/tree/main/v1">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
   </div>
+  <div class="features">
+    <ul>
+      <li>
+        <svg>
+          <use xlink:href="#icon-Vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>骄傲地使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-code"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>每个组件的源代码都极其简洁</p>
+      </li>
+    </ul>
+  </div>
+</div>
 </template>
+
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 export default {
-  components: { Topnav }
-}
+  components: {
+    Topnav
+  },
+};
 </script>
+
 <style lang="scss" scoped>
 $green: #02bcb0;
 $border-radius: 4px;
 $color: #007974;
 .topnavAndBanner {
-  background: linear-gradient(
-    145deg,
-    rgba(227, 255, 253, 1) 0%,
-    rgba(183, 233, 230, 1) 100%,
-  );
+  background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
-.features{
-  > svg{
-    width: 64px;
-    height: 64px;
+.features {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
+    }
   }
 }
 .banner {
@@ -55,15 +101,15 @@ $color: #007974;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  > .actions {
+  >.actions {
     padding: 8px 0;
     a {
       margin: 0 8px;
       background: $green;
       color: white;
       display: inline-block;
-      border-radius: $border-radius;
       padding: 8px 24px;
+      border-radius: $border-radius;
       &:hover {
         text-decoration: none;
       }
