acea67f3eb0c96ec834eeea402b8bc46ee06f370
gracale <acdepartmentwarning@gmail.com>
2021-12-12 00:12:50 +0800

    支持markdown

---
+++ v1/src/markdown/install.md
@@ -0,0 +1,10 @@
+# 安装
+打开终端运行下列命令:
+```
+npm install v1-ui
+```
+或
+```
+yarn add v1-ui
+```
+下一节：[开始使用](#/doc/get-started)
--- v1/src/views/Intro.vue
+++ v1/src/views/Intro.vue
@@ -1,9 +1,15 @@
 <template>
-<article class="markdown-body">
-  <h1>介绍</h1>
-  <p>King UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
-  <p>全是感情，没有技巧</p>
-  <p>所以强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。运行方法见 README.md。</p>
-  下一节:<a href="#/doc/install">安装</a>
+<article class="markdown-body" v-html="md">
 </article>
 </template>
+
+<script lang="ts">
+import md from '../markdown/intro.md';
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
