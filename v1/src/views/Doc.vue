a5a5aba0063af1398a8b1b6d590a9086c880817b
gracale <acdepartmentwarning@gmail.com>
2021-12-12 16:40:51 +0800

    debug doc

--- v1/src/views/Doc.vue
+++ v1/src/views/Doc.vue
@@ -81,9 +81,12 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
   height: 100%;
   z-index: $aside-index;
-
   > h2 {
     margin-bottom: 4px;
     padding: 0 16px;
@@ -99,15 +102,15 @@ aside {
         background: white;
       }
     }
-    @media (max-width: 500px) {
-      position: fixed;
-      top: 0;
-      left: 0;
-      padding-top: 70px;
-    }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 main {
   overflow: auto;
 }
-</style> 
+</style>
