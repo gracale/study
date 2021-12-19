7989a4e470a61aa56969feaad7245871a464f786
gracale <acdepartmentwarning@gmail.com>
2021-12-19 23:35:59 +0800

    new router

---
+++ notebook/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    data() {
+        return {
+            msg:'this is login page'
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}</style>
