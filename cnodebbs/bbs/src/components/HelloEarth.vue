e434e2d3c1eae3bcd49fcd28398b6c4f8dbfaf82
gracale <acdepartmentwarning@gmail.com>
2022-02-02 13:45:45 +0800

    up

---
+++ cnodebbs/bbs/src/components/HelloEarth.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>HelloEarth</h1>
+    <h2>Essential Links</h2>
+  <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloEarth',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+*{
+    color: red;
+}
+</style> 
---
+++ cnodebbs/bbs/src/components/list.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+       <li>
+           <router-link to="/HelloWorld">hello world</router-link>
+        </li> 
+       <li>
+           <router-link to="/HelloEarth">hello earth</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default{
+    name:"list"
+}
+</script>
+
+<style>
+
+</style>
