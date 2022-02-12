f6e2b10149ad3c4750a7c008bf983e46f2f868e2
gracale <acdepartmentwarning@gmail.com>
2022-02-13 01:26:21 +0800

    postlist组件

--- cnodebbs/cnode/src/App.vue
+++ cnodebbs/cnode/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <Header></Header>
+    <div class="main">
+      <PostList></PostList>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header'
+import PostList from './components/PostList'
 export default {
   name: 'App',
   components:{
-    Header
+    Header,PostList
   }
 }
 </script>
 
 <style>
-
+.main{
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
